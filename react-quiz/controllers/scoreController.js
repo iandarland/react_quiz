99e64f11038e3eea657da6035f58a1ff150db4a2
Ian Darland <ianpdarland@gmail.com>
2021-08-16 20:10:23 -0500

    routes hitting

--- react-quiz/controllers/scoreController.js
+++ react-quiz/controllers/scoreController.js
@@ -1,1 +1,10 @@
 const db = require("../models")
+
+module.exports = {
+    create: function(req, res) {
+        db.Score
+          .create(req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+      },
+}
--- react-quiz/routes/api/scores.js
+++ react-quiz/routes/api/scores.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-// const userController = require("../../controllers/userController");
+const scoreController = require("../../controllers/scoreController");
 
 router.route("/")
     .post(scoreController.create)
     
+module.exports = router;
